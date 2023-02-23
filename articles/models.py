f7de6d0c0419ad71ed2970aa0d104b46e5c5cba7
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-23 10:41:48 +0200

    Created models for articles app

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Article(models.Model):
+    topic = models.CharField(max_length=255, null=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(null=False)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+
+class Comments(models.Model):
+    comment = models.TextField(null=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Likes(models.Model):
+    like = models.BooleanField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
