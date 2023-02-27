3efcd18bbbed95ef495c561e1c0aacdfe216dbe7
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-27 13:27:34 +0200

    Logout

--- articles/urls.py
+++ articles/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'articles'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register/', views.register, name='register'),
+    path('register/', views.register_request, name='register'),
     path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout')
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 from .forms import NewUserForm
 
 
@@ -11,14 +12,15 @@ def index(request):
     return render(request, 'articles/index.html')
 
 
-def register(request):
+def register_request(request):
     if request.method == 'POST':
         form = NewUserForm(request.POST)
         if form.is_valid():
             user = form.save()
+            username = form.cleaned_data['username']
             login(request, user)
-            return render(request, 'articles/success_registration.html',
-                          {'username': form.cleaned_data['username']})
+            messages.info(request, f'Welcome to the Ligma, {username}')
+            return redirect('articles:index')
 
     elif request.method == 'GET':
         form = NewUserForm()
@@ -35,8 +37,15 @@ def login_request(request):
             user = authenticate(username=username, password=password)
             if user:
                 login(request, user)
-                return render(request, 'articles/success_login.html', {'username': username})
+                messages.info(request, f"Welcome back to Ligma, {username}")
+                return redirect('articles:index')
     else:
         form = AuthenticationForm()
 
     return render(request, 'articles/login.html', {'form': form})
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect('articles:index')
