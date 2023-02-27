df41ad629003274ff38410380495261524d6f29f
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-27 13:08:07 +0200

    Login in articles app

--- articles/urls.py
+++ articles/urls.py
@@ -6,4 +6,5 @@ app_name = 'articles'
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
+    path('login/', views.login_request, name='login'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.urls import reverse
-from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import NewUserForm
 
 
@@ -17,9 +17,26 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return HttpResponse(f'success {request.user}')
+            return render(request, 'articles/success_registration.html',
+                          {'username': form.cleaned_data['username']})
 
     elif request.method == 'GET':
         form = NewUserForm()
 
     return render(request, 'articles/registration.html', {'form': form})
+
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return render(request, 'articles/success_login.html', {'username': username})
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'articles/login.html', {'form': form})
