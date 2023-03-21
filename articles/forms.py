7696f0cd6f0cd252d26cb25fcab1fb5c5b204bd1
Andrew Subotin <subotinandrey5@gmail.com>
2023-03-21 12:07:38 +0200

    Deleted unnessecary code in forms

--- articles/forms.py
+++ articles/forms.py
@@ -19,35 +19,10 @@ class PublishArticleForm(forms.ModelForm):
         exclude = ['author', 'pub_date']
 
 
-# class UpdateArticleForm(forms.ModelForm):
-
-#     class Meta:
-#         model = Article
-#         exclude = ['author', 'pub_date',]
-
-#     def save(self):
-#         title = self.cleaned_data['title']
-#         content = self.cleaned_data['content']
-#         author = self.instance.author
-#         article_to_update = Article.objects.get(pk=self.instance.article)
-#         article_to_update.topic = title
-#         article_to_update.content = content
-#         article_to_update.author = author
-#         article_to_update.save()
-
-
 class LeaveCommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         exclude = ['commentator', 'article', 'pub_date']
-
-    # def save(self):
-    #     commentator = self.instance.commentator
-    #     article = self.instance.article
-    #     content = self.cleaned_data['content']
-    #     comment = Comment(commentator=commentator,
-    #                       article=article, content=content)
-    #     comment.save()
 
 
 class SearchForm(forms.Form):
